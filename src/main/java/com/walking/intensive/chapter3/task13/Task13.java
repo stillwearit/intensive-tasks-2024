b3d21f05ce1f3987e3602583ef7fdcd3ca80019f
Rita <margarita.mamatkazina@gmail.com>
2025-01-14 16:17:53 +0300

    пробуем

--- src/main/java/com/walking/intensive/chapter3/task13/Task13.java
+++ src/main/java/com/walking/intensive/chapter3/task13/Task13.java
@@ -61,7 +61,7 @@ public class Task13 {
         String plantsWatering = in.nextLine();
 
         String[] plantsWater = plantsWatering.split(" ");
-        int[] plants = new int [plantsWater.length];
+        int[] plants = new int[plantsWater.length];
         for (int i = 0; i < plants.length; i++) {
             plants[i] = parseInt(plantsWater[i]);
         }
@@ -78,6 +78,21 @@ public class Task13 {
             return -1;
         }
 
+        int stepsCount = 0;
+        int leftover = wateringCanVolume;
+        for (int i = 0; i < plants.length; i++) {
+            if (plants[i] < 1 || plants[i] > wateringCanVolume) {
+                return -1;
+            }
 
+            if (leftover >= plants[i]) {
+                stepsCount += 1;
+                leftover -= plants[i];
+            }
+
+            stepsCount += i * 2 + 1;
+        }
+
+        return stepsCount;
     }
 }
